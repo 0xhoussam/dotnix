5e7119e6e416e15dca3fd59f3768a6a4ea2deaf7
Houssam Abouiba <owner@0xhoussam.me>
2024-08-20 16:50:45 +0100

    chore: leaving .git in neovim config

--- shared/home-manager/tui/neovim.nix
+++ shared/home-manager/tui/neovim.nix
@@ -8,6 +8,7 @@
         repo = "neovim";
         rev = "6a9992fe00057a595e0a4ec0cd0a0faf9f31ca52";
         sha256 = "sha256-siMky/WFvhjnS1+t0zEx++omWv/QM3kbA431uTltJ8s=";
+        leaveDotGit = true;
       };
     };
   };
