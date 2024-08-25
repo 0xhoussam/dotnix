11f61ee4ce284a0699e5d60c1601325f01d9ef4d
Houssam Abouiba <owner@0xhoussam.me>
2024-08-25 03:16:03 +0100

    chore: setuping ibm plex font

--- homes/pride/fonts.nix
+++ homes/pride/fonts.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 {
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = [
+        "IBM Plex Serif"
+        "IBM Plex Sans Arabic"
+      ];
+      sansSerif = [
+        "IBM Plex Sans"
+        "IBM Plex Sans Arabic"
+      ];
+      monospace = [ "ZedMono Nerd Font" ];
+    };
+  };
   home.packages = with pkgs; [
     ibm-plex
     (nerdfonts.override { fonts = [ "ZedMono" ]; })
