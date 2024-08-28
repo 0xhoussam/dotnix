a8393b0d54d52f4f368e9ad205c60eedab41905c
Houssam Abouiba <owner@0xhoussam.me>
2024-08-28 02:58:24 +0100

    chore: adding greetd

--- hosts/dell/services.nix
+++ hosts/dell/services.nix
@@ -1,5 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.printing.enable = true;
   services.gvfs.enable = true; # for nautilus
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+      };
+    };
+  };
 }
