97a4e38bd2108f373fe715d5a4211e0ff8e484d2
0xhoussam <owner@0xhoussam.me>
2024-09-02 05:20:10 +0100

    chore(vscode): adding jetbrains icon theme ext

---
+++ shared/home-manager/gui/vscode-extensions/jetbrains-icon-theme.nix
@@ -0,0 +1,11 @@
+{
+  vscode-utils,
+}:
+vscode-utils.buildVscodeMarketplaceExtension {
+  mktplcRef = {
+    name = "vscode-jetbrains-icon-theme";
+    publisher = "chadalen";
+    version = "2.23.0";
+    sha256 = "02iynx0pjk1wdb9ws7n9bm8znpww4ssif3hby03siijkm3b2mvch";
+  };
+}
--- shared/home-manager/gui/vscode.nix
+++ shared/home-manager/gui/vscode.nix
@@ -1,18 +1,25 @@
 { pkgs, ... }:
 let
   fleet-theme = pkgs.callPackage ./vscode-extensions/Jetbrains-fleettheme.nix { };
+  jetbrains-icon-theme = pkgs.callPackage ./vscode-extensions/jetbrains-icon-theme.nix { };
 in
 {
   programs.vscode = {
     enable = true;
     extensions = with pkgs; [
       vscode-extensions.vscodevim.vim
+      jetbrains-icon-theme
       fleet-theme
     ];
     userSettings = {
       "window.titleBarStyle" = "custom";
       "editor.fontSize" = 18;
       "editor.fontFamily" = "monospace";
+      "workbench.colorTheme" = "JetBrains Fleet";
+      "editor.lineNumbers" = "relative";
+      "workbench.list.smoothScrolling" = true;
+      "editor.tabSize" = 4;
+      "workbench.iconTheme" = "catppuccin-mocha";
     };
   };
 }
