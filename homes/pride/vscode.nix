40db2c426e0b64c7cd030fe6d49491248e805c77
0xhoussam <owner@0xhoussam.me>
2024-09-02 02:06:19 +0100

    chore(vscode) chaning font

--- homes/pride/vscode.nix
+++ homes/pride/vscode.nix
@@ -5,6 +5,8 @@
     extensions = with pkgs; [ vscode-extensions.vscodevim.vim ];
     userSettings = {
       "window.titleBarStyle" = "custom";
+      "editor.fontSize" = 18;
+      "editor.fontFamily" = "monospace";
     };
   };
 }
