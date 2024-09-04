df46cceb8472b105baf58681b132db5422099e54
0xhoussam <owner@0xhoussam.me>
2024-09-04 03:30:01 +0100

    chore(xdg): removing wlr portal

--- hosts/dell/xdg.nix
+++ hosts/dell/xdg.nix
@@ -6,7 +6,6 @@
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      xdg-desktop-portal-wlr
       xdg-desktop-portal-hyprland
     ];
     xdgOpenUsePortal = true;
