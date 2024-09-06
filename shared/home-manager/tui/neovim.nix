a64aff78904fd4624073139207d3d0ce727ebe83
0xhoussam <owner@0xhoussam.me>
2024-09-06 03:44:02 +0100

    chore: adding neovim and commenting the config

--- shared/home-manager/tui/neovim.nix
+++ shared/home-manager/tui/neovim.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 {
   programs.neovim.enable = true;
-  home.file = {
-    ".config/nvim" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "0xhoussam";
-        repo = "neovim";
-        rev = "88977866d702586d0f119ab4a125f1f676435d88";
-        sha256 = "sha256-zeqHwvmwpXZhG5pzkP5VKt7zDOu/YGu0RiB6eYwMJvM=";
-      };
-    };
-  };
+  # home.file = {
+    # ".config/nvim" = {
+      # source = pkgs.fetchFromGitHub {
+        # owner = "0xhoussam";
+        # repo = "neovim";
+        # rev = "88977866d702586d0f119ab4a125f1f676435d88";
+        # sha256 = "sha256-zeqHwvmwpXZhG5pzkP5VKt7zDOu/YGu0RiB6eYwMJvM=";
+      # };
+    # };
+  # };
 }
