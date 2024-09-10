08af8543dc166fb4dee743cbf9bb9d1381c9022b
0xhoussam <owner@0xhoussam.me>
2024-09-10 10:36:38 +0100

    chore: adding git aliases

--- shared/home-manager/tui/git.nix
+++ shared/home-manager/tui/git.nix
@@ -22,6 +22,43 @@
       branch.sort = "-committerdate";
       color.ui = "auto";
       init.defaultBranch = "main";
+      aliases = {
+        # add
+        a = "add";
+        aa = "add -all";
+
+        # commit
+        c = "commit";
+        cm = "commit -m";
+        cam = "commit -am";
+
+        # status
+        s = "status";
+
+        # push
+        p = "push";
+
+        # pull
+        pl = "pull";
+
+        # clone
+        cl = "clone";
+        cls = "clone --depth=1"; # shallow clone
+
+        # diff
+        d = "diff";
+        ds = "diff --staged";
+
+        # log
+        l = "log";
+        lo = ''log --pretty=format:"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]" --decorate --date=short'';
+
+        # stash
+        staash = "stash --all";
+
+        # branch
+        b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
+      };
     };
   };
 }
