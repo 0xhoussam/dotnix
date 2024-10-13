806b30352c079b9bbdbd5f4024d0040487bb8278
0xhoussam <owner@0xhoussam.me>
2024-10-13 21:14:51 +0100

    chore: adding auto-cpufreq to dell host

--- hosts/dell/services.nix
+++ hosts/dell/services.nix
@@ -1,6 +1,17 @@
-{ pkgs, ... }:
+{ ... }:
 {
   services.printing.enable = true;
   services.gvfs.enable = true;
   services.flatpak.enable = true;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
 }
