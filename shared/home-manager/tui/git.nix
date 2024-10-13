1562f1081db81a3ea8559ab9e73b70d067358ec4
0xhoussam <owner@0xhoussam.me>
2024-10-13 21:25:23 +0100

    chore: fixing git aliases

--- shared/home-manager/tui/git.nix
+++ shared/home-manager/tui/git.nix
@@ -22,45 +22,43 @@
       branch.sort = "-committerdate";
       color.ui = "auto";
       init.defaultBranch = "main";
-      aliases = {
-        # add
-        a = "add";
-        aa = "add -all";
+    };
+    aliases = {
+      # add
+      a = "add";
+      aa = "add -all";
 
-        # commit
-        c = "commit";
-        cm = "commit -m";
-        cam = "commit -am";
+      # commit
+      c = "commit";
+      cm = "commit -m";
+      cam = "commit -am";
 
-        # status
-        s = "status";
+      # status
+      s = "status";
 
-        # push
-        p = "push";
+      # push
+      p = "push";
 
-        # pull
-        pl = "pull";
+      # pull
+      pl = "pull";
 
-        # clone
-        cl = "clone";
-        cls = "clone --depth=1"; # shallow clone
+      # clone
+      cl = "clone";
+      cls = "clone --depth=1"; # shallow clone
 
-        # diff
-        d = "diff";
-        ds = "diff --staged";
+      # diff
+      d = "diff";
+      ds = "diff --staged";
 
-        # log
-        l = "log";
-        lo = ''
-          git log --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'
-        '';
+      # log
+      l = "log";
+      lo = "log --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'";
 
-        # stash
-        staash = "stash --all";
+      # stash
+      staash = "stash --all";
 
-        # branch
-        b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
-      };
+      # branch
+      b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
     };
   };
 }
