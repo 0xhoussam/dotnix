78189e07da203dbf4a3df4a96bc4bdd08a600a0e
0xhoussam <owner@0xhoussam.me>
2024-11-21 16:56:27 +0100

    feat(nix): add caches

--- flake.nix
+++ flake.nix
@@ -1,5 +1,13 @@
 {
   description = "A simple NixOS flake";
+  nixConfig = {
+    extra-substituters = [
+      "https://colmena.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+    ];
+  };
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "nixpkgs/nixos-unstable";
--- shared/nixos/nix.nix
+++ shared/nixos/nix.nix
@@ -11,16 +11,20 @@
 
     settings = {
       sandbox = true;
-
       experimental-features = [
         "nix-command"
         "flakes"
         "read-only-local-store"
       ];
-
       trusted-users = [ "@wheel" ];
-
       warn-dirty = false;
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
     };
   };
 }
