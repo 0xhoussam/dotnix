2a6b9aed684906c8250a3e8a3398b68d61822291
0xhoussam <owner@0xhoussam.me>
2025-02-16 15:43:15 +0100

    feat(pgks): add zoxide and yazi

--- homes/pride/pkgs.nix
+++ homes/pride/pkgs.nix
@@ -48,6 +48,8 @@ in
     wget
     wl-clipboard
     zip
+    yazi
+    zoxide
 
     (writeShellScriptBin "reload-failed-services" ''
       systemctl --user list-units --failed | grep -Po '([A-Za-z-0-9]+.service)' | xargs systemctl --user restart
