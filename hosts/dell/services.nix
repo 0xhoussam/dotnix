b80cc48e257e4eef582fbd9ccbfbddf92d0d43ce
0xhoussam <owner@0xhoussam.me>
2025-02-18 19:30:06 +0100

    feat(dell): replace cpufreq with cpupower

--- hosts/dell/services.nix
+++ hosts/dell/services.nix
@@ -2,17 +2,18 @@
 {
   services.gvfs.enable = true;
   services.flatpak.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-      governor = "powersave";
-      turbo = "never";
-    };
-    charger = {
-      governor = "performance";
-      turbo = "auto";
-    };
-  };
+  services.cpupower-gui.enable = true;
+  # services.auto-cpufreq.enable = true;
+  # services.auto-cpufreq.settings = {
+  #   battery = {
+  #     governor = "powersave";
+  #     turbo = "never";
+  #   };
+  #   charger = {
+  #     governor = "performance";
+  #     turbo = "auto";
+  #   };
+  # };
   services.preload.enable = true;
   services.greetd = {
     enable = true;
